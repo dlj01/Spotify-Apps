71b20784e02bcd61d4f2c0b9970ceee6e94437fc
dljones01 <djones01@vt.edu>
2026-05-13 19:02:57 -0400

    added

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def callback():
 
 @app.route("/logout")
 def logout():
-    tokens = sp_module._load_tokens()
+    tokens = sp_module._load_tokens("d")
     tokens.pop("top_tracks", None)
     sp_module._save_tokens(tokens)
     return redirect("/")
